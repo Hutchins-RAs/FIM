53f8befd6977fc6f5e24d36a84678f96502414c9
The Hutchins Center at Brookings <60798139+hutchinsra@users.noreply.github.com>
2020-11-16 17:29:53 -0500

    Documentation of haver and cbo data

---
+++ R/budget-projections.R
@@ -0,0 +1,19 @@
+
+#' CBO 10-Year Budget Projections
+#'
+#' Projections of spending and revenues by category and of deficits and debt held by the public.
+#'
+#' @format A data frame with 56 rows and 10 variables:
+#' \describe{
+#'   \item{\code{fy}}{double. DESCRIPTION.}
+#'   \item{\code{gftfp}}{double. DESCRIPTION.}
+#'   \item{\code{gfrpt}}{double. DESCRIPTION.}
+#'   \item{\code{gfrpri}}{double. DESCRIPTION.}
+#'   \item{\code{gfrcp}}{double. DESCRIPTION.}
+#'   \item{\code{gfrs}}{double. DESCRIPTION.}
+#'   \item{\code{yptmr}}{double. DESCRIPTION.}
+#'   \item{\code{yptmd}}{double. DESCRIPTION.}
+#'   \item{\code{yptu}}{double. DESCRIPTION.}
+#'   \item{\code{date}}{double. DESCRIPTION.}
+#' }
+"budg"
---
+++ R/economic-projections-data.R
@@ -0,0 +1,26 @@
+#' CBO Quarterly Economic projections
+#'
+#' Projections of output, prices, labor market measures,
+#' interest rates, and income. Beginning in January 2020, these files also include projections of
+#' potential GDP (the economy’s maximum sustainable output) and its underlying inputs
+#'
+#' @format A data frame with 56 rows and 16 variables:
+#' \describe{
+#'   \item{\code{date}}{double. DESCRIPTION.}
+#'   \item{\code{gdp}}{double. DESCRIPTION.}
+#'   \item{\code{gdph}}{double. DESCRIPTION.}
+#'   \item{\code{gdppothq}}{double. DESCRIPTION.}
+#'   \item{\code{gdppotq}}{double. DESCRIPTION.}
+#'   \item{\code{dc}}{double. DESCRIPTION.}
+#'   \item{\code{jgdp}}{double. DESCRIPTION.}
+#'   \item{\code{c}}{double. DESCRIPTION.}
+#'   \item{\code{ch}}{double. DESCRIPTION.}
+#'   \item{\code{gh}}{double. DESCRIPTION.}
+#'   \item{\code{gfh}}{double. DESCRIPTION.}
+#'   \item{\code{gsh}}{double. DESCRIPTION.}
+#'   \item{\code{g}}{double. DESCRIPTION.}
+#'   \item{\code{gf}}{double. DESCRIPTION.}
+#'   \item{\code{gs}}{double. DESCRIPTION.}
+#'   \item{\code{cpiu}}{double. DESCRIPTION.}
+#' }
+"econ"
