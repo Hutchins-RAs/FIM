fdc3ba2c413ce0281654cae4b7669b761363fcd8
The Hutchins Center at Brookings <60798139+hutchinsra@users.noreply.github.com>
2021-01-09 11:35:25 -0500

    Update load.R

--- R/load.R
+++ R/load.R
@@ -1,3 +1,79 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+load_economic_projections <- function(){
+  read_xlsx(here('data/raw/cbo/cbo_econ_proj_quarterly.xlsx')) %>%
+    mutate(date = as_date(date))
+}
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+load_budget_projections <- function(){
+  read_xlsx(here('data/raw/cbo/cbo_budget_nipas_proj_annual.xlsx')) %>%
+    as_tsibble(index = fy)
+}
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+load_cbo_projections <- function(){
+  budget <- load_budget_projections()
+  economic <- load_economic_projections()
+  
+  budget %>%
+    annual_to_quarter(fy) %>%
+    left_join(economic)
+}
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+load_unemployment_insurance_override <- function(){
+  read_excel("documentation/COVID-19 Changes/September/LSFIM_KY_v6_round2.xlsx", 
+             sheet = "FIM Add Factors") %>%
+    mutate(date = as_date(date)) %>%
+    select(date, contains('unemployment_insurance'))
+}
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_haver_data <- function(){
+  # Load U.S. national accounts and economic statistics data into the Global Environment
+  haver_data_path <-
+    here('data/raw/haver/')
+  haver_data_names <- 
+    haver_data_path  %>%
+    list.files() %>%
+    .[str_detect(., ".xlsx")]
+  
+  # Load raw Haver data into global environment
+  haver_data_names %>%  
+    purrr::map(function(file_name){ # iterate through each file name
+      assign(x = str_remove(file_name, ".xlsx"), 
+             value = read_xlsx(paste0(haver_data_path,"/", file_name), na = 'NA') %>%
+               mutate(date = as.Date(date)),
+             envir = .GlobalEnv)
+    }) 
+  # Merge quarterly and annual data 
+  # change hist and aa to haver quarterly and annual
+  left_join(national_accounts,
+            economic_statistics,
+            by = "date") 
+}
+
 #' Load contributions used in figures
 #'
 #' @return
