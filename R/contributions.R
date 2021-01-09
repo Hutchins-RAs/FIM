673e1431c2ff617ec0531c7977505d7e77653395
The Hutchins Center at Brookings <60798139+hutchinsra@users.noreply.github.com>
2021-01-09 01:38:14 -0500

    extract_social_benefits_components.R

--- R/contributions.R
+++ R/contributions.R
@@ -32,32 +32,7 @@ total_purchases <- function(df){
            purchases_cont = federal_cont + state_local_cont)
 }
 
-#' Separate unemployment insurance and rebate checks from social benefits
-#'
-#' @param df 
-#' @param component 
-#'
-#' @return
-#' @export
-#'
-#' @examples
-remove_social_benefit_components <- function(df, component){
-  remove_unemployment_insurance <- function(df){
-    df %>%
-      mutate(social_benefits = social_benefits - unemployment_insurance,
-             federal_social_benefits = federal_social_benefits - federal_unemployment_insurance,
-             state_social_benefits = state_social_benefits - state_unemployment_insurance)
-  }
-  remove_rebate_checks <- function(df){
-    df %>%
-      mutate(social_benefits = social_benefits - rebate_checks,
-             federal_social_benefits = federal_social_benefits - federal_rebate_checks,
-             state_social_benefits = state_social_benefits - state_rebate_checks)
-  }
-  df %>%
-    remove_unemployment_insurance() %>%
-    remove_rebate_checks()
-}
+
 #' Calculate neutral counterfactual taxes and transfers
 #'
 #' @param x 
---
+++ R/extract_social_benefits_components.R
@@ -0,0 +1,53 @@
+#' Separate unemployment insurance and rebate checks from social benefits
+#'
+#' @param df 
+#' @param component 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+remove_social_benefit_components <- function(df, component){
+  remove_unemployment_insurance <- function(df){
+    df %>%
+      mutate(social_benefits = social_benefits - unemployment_insurance,
+             federal_social_benefits = federal_social_benefits - federal_unemployment_insurance,
+             state_social_benefits = state_social_benefits - state_unemployment_insurance)
+  }
+  remove_rebate_checks <- function(df){
+    df %>%
+      mutate(social_benefits = social_benefits - rebate_checks,
+             federal_social_benefits = federal_social_benefits - federal_rebate_checks,
+             state_social_benefits = state_social_benefits - state_rebate_checks)
+  }
+  df %>%
+    remove_unemployment_insurance() %>%
+    remove_rebate_checks()
+}
+#' Title
+#'
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+add_social_benefit_components <- function(df){
+  add_unemployment_insurance <- function(df){
+    df %>%
+      mutate(social_benefits = social_benefits + unemployment_insurance,
+             federal_social_benefits = federal_social_benefits + federal_unemployment_insurance,
+             state_social_benefits = state_social_benefits + state_unemployment_insurance)
+  }
+  
+  add_rebate_checks <-function(df){
+    df %>%
+      mutate(social_benefits = social_benefits + rebate_checks,
+             federal_social_benefits = federal_social_benefits + federal_rebate_checks,
+             state_social_benefits = state_social_benefits + state_rebate_checks)
+  }
+  
+  df %>%
+    add_unemployment_insurance() %>%
+    add_rebate_checks()
+}
