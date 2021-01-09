db64bcc7171ac25d102e7ddb2041549f79b5e1c5
The Hutchins Center at Brookings <60798139+hutchinsra@users.noreply.github.com>
2021-01-09 15:52:03 -0500

    Update make_forecast.R

--- R/make_forecast.R
+++ R/make_forecast.R
@@ -4,8 +4,8 @@
 #' @export
 #'
 #' @examples
-make_forecast <- function(){
-  for(f in get_forecast_period()){
+make_forecast <- function() {
+  for(f in get_forecast_period()) {
     prepare_projections[f,components] = prepare_projections[f-1, components]  * (1 + prepare_projections[f, paste0(components, "_g")])
   }
 }
