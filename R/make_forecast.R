9a214250e5fe4852a77004b989a12c85f779f1bb
The Hutchins Center at Brookings <60798139+hutchinsra@users.noreply.github.com>
2021-01-10 03:18:12 -0500

    make forecast

--- R/make_forecast.R
+++ R/make_forecast.R
@@ -23,8 +23,58 @@ get_forecast_period <- function(df, last_historical_date){
 #' @export
 #'
 #' @examples
-make_forecast <- function(df, forecast_period, components) {
-  for(f in forecast_period) {
-    df[f,components] = df[f-1, components]  * (1 + df[f, paste0(components, "_g")])
-  }
+make_forecasts <- function(df){
+  df %>%
+    forecast_series(gdp)%>%
+    forecast_series(gdph) %>%
+    forecast_series(jgdp) %>%
+    forecast_series(gdppotq) %>%
+    forecast_series(gdppothq) %>%
+    forecast_series(gs) %>%
+    forecast_series(gf) %>%
+    forecast_series(jgf) %>%
+    forecast_series(jgs) %>%
+    forecast_series(jgse) %>%
+    forecast_series(jgsi) %>%
+    forecast_series(gfeg) %>%
+    forecast_series(gfeghhx) %>%
+    forecast_series(gfeghdx) %>%
+    forecast_series(gfeigx) %>%
+    forecast_series(gfrpt) %>%
+    forecast_series(gsrpt) %>%
+    forecast_series(gfrs) %>%
+    forecast_series(gsrs) %>%
+    forecast_series(gfrcp) %>%
+    forecast_series(gsrcp) %>%
+    forecast_series(gfrpri) %>%
+    forecast_series(gsrpri) %>%
+    forecast_series(gftfp) %>%
+    forecast_series(gstfp) %>%
+    forecast_series(yptmd) %>%
+    forecast_series(yptmr) %>%
+    forecast_series(gssub) %>%
+    forecast_series(gfsub) %>%
+    forecast_series(c) %>%
+    forecast_series(jc)
+}
+
+#' Title
+#'
+#' @param df 
+#' @param comp 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+forecast_series <- function(df, comp){
+  comp <- ensym(comp)
+  comp_string <- rlang::as_string(enexpr(comp))
+  comp_cum_growth_string <- paste0(comp_string, '_g_cumulative_growth')
+  comp_cum_growth <- rlang::sym(comp_cum_growth_string)
+  df %>%
+    mutate('{{comp}}' := if_else(date > last_hist_date,
+                                 lag({{comp}}) * !!(comp_cum_growth),
+                                 {{comp}})
+    ) 
 }
