b894c152c6d5539cfe5aed288352bf4ff2fb57f3
Manuel  Alcala Kovalski <malcalakovalski@brookings.edu>
2021-08-12 14:56:09 -0400

    Automate fetching/pulling on master_script.R

--- master_script.R
+++ master_script.R
@@ -5,6 +5,12 @@ setwd("C:/Users/scampbell/Documents/GitHub/fim")
 #Pull the most recent changes from GitHub
 #________
 #Loading the FIM package
+librarian::shelf('gert')
+# Check whether any changes have been made
+git_fetch()
+# Pull those changes onto your computer
+git_pull()
+
 devtools::load_all(".")
 #Running the FIM calculations
 source('fiscal_impact.R')
