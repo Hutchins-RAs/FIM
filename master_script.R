c240e75a9f2349185c83aa48e7839c65d343dd5e
Manuel  Alcala Kovalski <malcalakovalski@brookings.edu>
2021-08-24 17:53:31 +0000

    Create results/08/

--- master_script.R
+++ master_script.R
@@ -4,6 +4,11 @@
 setwd("C:/Users/scampbell/Documents/GitHub/fim")
 #Pull the most recent changes from GitHub
 #________
+# Packages to install on the first day:
+install.packages('librarian')
+librarian::shelf('malcalakovalski/fim', 'here', 'roll', 'TimTeaFan/
+dplyover')
+
 #Loading the FIM package
 librarian::shelf('gert', 'tidyverse', 'glue')
 # Check whether any changes have been made
