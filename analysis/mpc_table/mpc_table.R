56ec01cf6216c7dadf67ebc1193666524939366e
malcalakovalski <malcalakovalski@brookings.edu>
2021-12-23 11:59:44 -0500

    Q3 third revision update

--- analysis/mpc_table/mpc_table.R
+++ analysis/mpc_table/mpc_table.R
@@ -4,7 +4,6 @@ librarian::shelf(tidyverse, gt)
 my_theme <- function(data, ...){
   data %>%
     opt_row_striping() %>%
-    opt_all_caps() %>%
     opt_table_font(
       font = list(
         google_font("Roboto"),
@@ -55,8 +54,7 @@ my_theme <- function(data, ...){
 mpc_data_raw <- readxl::read_xlsx('data/forecast.xlsx', sheet = 'mpc',
                                range = "B2:N14")
 
-order <- c('social_benefits', 'health_outlays', 'ui', 'ui_arp', 'rebate_checks', 'rebate_checks_arp', 'other_direct_aid_arp', 'other_vulnerable_arp', 'subsidies', 'aid_to_small_businesses_arp', 'non_corporate_taxes', 'corporate_taxes')
-
+order <- c('ui', 'rebate_checks', 'subsidies', 'ui_arp', 'other_vulnerable_arp',  'rebate_checks_arp','aid_to_small_businesses_arp', 'other_direct_aid_arp', 'social_benefits', 'health_outlays', 'non_corporate_taxes', 'corporate_taxes')
 
 # Cleaning ----------------------------------------------------------------
 
@@ -70,19 +68,21 @@ mpc_data <-
   arrange(desc(variable))
 
 mpc_descriptions <-
-  tribble(~variable, ~description,
-        'social_benefits', "Effect occurs smoothly in the first year.",
-        'health_outlays', "Effect occurs smoothly in the first year.",
-        'ui', "70 percent of the effect occur within the first two quarters and the remainder is spread out over four quarters at a decreasing rate.",
-        'ui_arp', "Roughly 75 percent of the effect ocurrs within the first year and 25 percent in the second year.",
-        'rebate_checks', "Half of the effect ocurrs within the first two quarters and then smoothly over six quarters.",
-        'rebate_checks_arp', "Roughly 35 percent of the effect ocurrs within three quarters and then smoothly over six quarters.",
-        'other_direct_aid_arp', "We apply the same timing as to the ARP rebate checks.",
-        'other_vulnerable_arp', "We apply the same timing as to the ARP unemployment insurance expansion.",
-        'subsidies', "Effect occurs at a slowly decreasing rate over three years.",
-        'aid_to_small_businesses_arp', "Effect occurs at a slowly decreasing rate over three years.",
-        'non_corporate_taxes', "60 percent of the effect ocurrs in the first year and 40 percent in the second.",
-        'corporate_taxes', "Effect occurs smoothly over three years.")
+  tribble(~variable, ~group, ~description,
+  'ui', 'Legislation in 2020 (Cares Act, Families First,  December 2020 legislation and other)',"70% of the effect occurs within the first two quarters and the remainder is spread out over four quarters at a decreasing rate.",
+  'rebate_checks','Legislation in 2020 (Cares Act, Families First,  December 2020 legislation and other)', "Half of the effect occurs within the first two quarters and then smoothly over six quarters.",
+  'subsidies','Legislation in 2020 (Cares Act, Families First,  December 2020 legislation and other)',"Effect occurs at a slowly decreasing rate over three years.",
+  'ui_arp', 'American Rescue Plan (ARP)', "Roughly 35% of the effect occurs within two quarters and the remainder is spread out over six quarters at a decreasing rate.",
+  'other_vulnerable_arp','American Rescue Plan (ARP)', "We apply the same timing as to the ARP unemployment insurance expansion.",
+  'rebate_checks_arp','American Rescue Plan (ARP)', "Roughly 35% of the effect occurs within three quarters and then smoothly over six quarters.",
+  'aid_to_small_businesses_arp','American Rescue Plan (ARP)', "Effect occurs at a slowly decreasing rate over three years.",
+  'other_direct_aid_arp','American Rescue Plan (ARP)', "We apply the same timing as to the ARP rebate checks.",
+  'social_benefits', 'Prior to 2020', "Effect occurs smoothly in the first year.",
+  'health_outlays', 'Prior to 2020', "Effect occurs smoothly in the first year.",
+  'non_corporate_taxes', 'Prior to 2020', "60% of the effect occurs in the first year and 40% in the second.",
+  'corporate_taxes', 'Prior to 2020', "Effect occurs smoothly over three years."
+)
+
 
 mpc_table <-
   mpc_data %>% 
@@ -90,46 +90,38 @@ mpc_table <-
   mutate(variable = snakecase::to_title_case(variable),
          variable = stringr::str_replace(variable, 'Arp', 'ARP'),
          variable = stringr::str_replace(variable, 'Ui', "Unemployment Insurance"),
-         variable = str_replace(variable, "Unemployment Insurance ARP", "ARPA Unemployment Insurance"),
-         variable = str_replace(variable, "Other Direct Aid ARP", "Other ARPA Direct Aid to Families"),
-         variable = str_replace(variable, "Other Vulnerable ARP", "Other ARPA Aid to Financially Vulnerable Families"),
-         variable = str_replace(variable, "Aid to Small Businesses ARP", "ARPA Subsidies"),
-         variable = str_replace(variable,  "Rebate Checks ARP", "ARPA Rebate Checks"))
+         variable = str_replace(variable, "Unemployment Insurance ARP", "Unemployment Insurance and Other Aid to Financially Vulnerable Families (e.g., SNAP, housing assistance)"),
+         variable = str_replace(variable, "Other Direct Aid ARP", "Other Direct Aid to Families (e.g., Child Tax Credit, Earned Income Tax Credit)"),
+         variable = str_replace(variable, 
+                                'Subsidies', 
+                                'Subsidies (mostly PPP)'),
+         variable = str_replace(variable, "Other Vulnerable ARP", "Other Aid to Financially Vulnerable Families (e.g., SNAP, housing assistance)"),
+         variable = str_replace(variable, "Aid to Small Businesses ARP", "Subsidies (mostly PPP)"),
+         variable = str_replace(variable,  "Rebate Checks ARP", "Rebate Checks and Other Direct Aid to Families (e.g., Child Tax Credit, Earned Income Tax Credit)"),
+         variable = str_replace(variable,  "Non Corporate Taxes", "Non-Corporate Taxes")) %>% 
+  relocate('group', .before = 'variable')
 # Table -------------------------------------------------------------------
 
 
-new_order <- c(
-  "Social Benefits",
-  "Health Outlays",
-  "Unemployment Insurance",
-  "ARPA Unemployment Insurance",
-  "Rebate Checks",
-  "ARPA Rebate Checks",
-  "Other ARPA Direct Aid to Families",
-  "Other ARPA Aid to Financially Vulnerable Families",
-  "Subsidies",
-  "ARPA Subsidies",
-  "Non Corporate Taxes",
-  "Corporate Taxes"
-)
-base_table <-
   mpc_table %>% 
-  mutate(variable = as_factor(variable),
-         variable = fct_relevel(variable, rev(new_order))) %>% 
-  arrange(desc(variable)) %>% 
-  relocate(description, .after = 'mpc') %>% 
-  gt() %>% 
+  filter(variable != 'Health Outlays',
+         variable != 'Other Aid to Financially Vulnerable Families (e.g., SNAP, housing assistance)',
+         variable != 'Other Direct Aid to Families (e.g., Child Tax Credit, Earned Income Tax Credit)') %>% 
+  mutate(variable = str_replace(variable,
+                                'Social Benefits',
+                                'Social Benefits and Health Outlays')) %>% 
+  gt(groupname_col = 'group',
+     rowname_col = 'variable') %>% 
   my_theme() %>% 
   fmt_percent(mpc, decimals = 0) %>% 
   tab_header(title = md("Assumed MPCs")) %>% 
-  cols_align(align = 'right',
+  cols_align(align = 'center',
              columns = 'mpc') %>% 
   cols_align(align = 'left',
              c(variable, description)) %>% 
   cols_label(variable = 'Component',
              description = '',
-             mpc = 'Total MPC')
+             mpc = 'Total MPC') %>% 
+  cols_width(mpc ~ px(100)) -> table
 
-table <-
-  base_table 
 gtsave(table, 'analysis/mpc_table/mpc_table.png')
