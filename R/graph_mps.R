bbcaa3e6ef027873ca8e789b00633a3751121ebf
lorae-brookings <124624026+lorae-brookings@users.noreply.github.com>
2023-05-20 07:51:47 +1100

    Update graph_mps.R
    Created a line chart version of disbursed versus implied savings. Next step: do cumulative savings

--- R/graph_mps.R
+++ R/graph_mps.R
@@ -10,6 +10,7 @@
 #
 # SECTION 1: graph_mps function
 # SECTION 2: ss_graph_wrapper function
+# SECTION 3: line_mps function
 
 ########## SECTION 1: graph_mps function ##########
 graph_mps <- function(disbursed = projections$federal_ui, # How much $ was actually disbursed
@@ -78,11 +79,31 @@ graph_mps <- function(disbursed = projections$federal_ui, # How much $ was actua
     # Remove the legend title
     guides(fill = guide_legend(title = NULL)) 
   
+  # Generate line chart using ggplot2
+  line_chart <- ggplot(combined_df, aes(x = Date + x_offset, y = Value, color = Dataset)) + 
+    geom_line(aes(group = Dataset), size = 1) +
+    geom_point(aes(shape = Dataset), size = 3) +
+    scale_x_date(date_labels = "%Y", date_breaks = "1 year", limits = c(start_date, end_date)) +
+    scale_y_continuous(label = scales::dollar_format(suffix = " B")) +
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 0,
+                                     vjust = 0.5,
+                                     hjust = 0.5),
+          panel.grid.major.y = element_line(color = "grey60", linetype = "dashed"),
+          plot.caption = element_text(hjust = 0)) + 
+    labs(title = custom_title,
+         x = "", 
+         y = "", 
+         caption = custom_caption) + 
+    scale_color_manual(values = c("Disbursed" = "blue", "Implied Saving" = "orange")) +
+    scale_shape_manual(values = c("Disbursed" = 20, "Implied Saving" = 20)) +
+    guides(color = guide_legend(title = NULL), shape = guide_legend(title = NULL))
+  
   # # Display the bar chart
   # print(bar_chart)
   
   # Return both the data frame and the chart
-  return(list(data = combined_df, chart = bar_chart))
+  return(list(data = combined_df, bar = bar_chart, line = line_chart))
 }
 
 ########## SECTION 2: ss_graph_wrapper function ##########
@@ -92,7 +113,9 @@ ss_graph_wrapper <- function(disbursed = projections$federal_other_vulnerable_ar
                              date = projections$date, # A vector of the dates used in the graph
                              start = "2019-01-01", # Graph start date
                              end = "2025-01-01", # Graph end date
-                             title = "Federal Other Vulnerable ARP") # Graph title
+                             title = "Federal Other Vulnerable ARP", # Graph title
+                             annualized = TRUE # TRUE if data is annualized
+)
   {
   # First, get the MPS vector using mps_name
   mps <- c_mps[which(alt_mps$variable == mps_name),] %>%
@@ -125,8 +148,83 @@ ss_graph_wrapper <- function(disbursed = projections$federal_other_vulnerable_ar
               start = start, # Graph start date
               end = end, # Graph end date
               title = title, # Graph title
-              terminal = terminal) # Terminal MPS
+              terminal = terminal, # Terminal MPS
+              annualized = annualized # TRUE if data are annualized
+  )
+  
 
   # Return the result
   return(result)
 }
+
+########## SECTION 3: line_mps function ##########
+
+### Obsolete
+
+line_mps <- function(disbursed = projections$federal_ui, # How much $ was actually disbursed
+                      ss = ss_federal_ui_arp, # Our best guesses on savings resulting from disbursement
+                      date = projections$date, # A vector of the dates used in the graph
+                      start = "2019-01-01", # Graph start date
+                      end = "2025-01-01", # Graph end date
+                      title = "Test data type", # Graph title
+                      terminal, # Terminal MPS for graph caption
+                      annualized = TRUE # TRUE if values are annualized
+) {
+  
+  # Turn annualized values into quarterly values
+  if(annualized == TRUE) {
+    disbursed <- disbursed/4
+    ss <- ss/4
+  }
+  # Assemble the data for graphing
+  combined_df <- data.frame(
+    # Constructing the "Date" column
+    Date = as.Date(date), 
+    # Constructing a column that includes disbursed and saving
+    Value = c(disbursed, ss),
+    # Constructing a column that labels observations as either a disbursement or
+    # implied saving
+    Dataset = rep(c("Disbursed", "Implied Saving"), c(length(disbursed), length(ss)))
+  )
+  
+  # Initialize graph settings
+  # Construct the graph title
+  custom_title <- paste0(title, 
+                         "\nDisbursement (using BLS data) versus Implied Saving (using MPC assumptions)")
+  # Construct the graph caption
+  custom_caption <- paste0("Note: Terminal MPS is ", terminal*100, "% of initial disbursement.")
+  # Create custom colors
+  translucent_blue <- rgb(0, 0.3, 1, alpha = 0.1)
+  translucent_orange <- rgb(1, 0.7, 0, alpha = 0.1)
+  # Set the date range for display
+  start_date <- as.Date(start) # first bar is 2019 Q1
+  end_date <- as.Date(end) # last bar is 2024 Q4
+  # Add an offset to the x-axis positions. This will align the bars so that 
+  # the 2021 Q1 bar, for example, will sit to the right of the 2021 tick mark, 
+  # rather than be aligned with the center of the tick mark (which is the default
+  # result).
+  x_offset <- 45
+  
+  # Generate line chart using ggplot2
+    line_chart <- ggplot(combined_df, aes(x = Date + x_offset, y = Value, color = Dataset)) + 
+      geom_line(aes(group = Dataset), size = 1) +
+      geom_point(aes(shape = Dataset), size = 3) +
+      scale_x_date(date_labels = "%Y", date_breaks = "1 year", limits = c(start_date, end_date)) +
+      scale_y_continuous(label = scales::dollar_format(suffix = " B")) +
+      theme_classic() +
+      theme(axis.text.x = element_text(angle = 0,
+                                       vjust = 0.5,
+                                       hjust = 0.5),
+            panel.grid.major.y = element_line(color = "grey60", linetype = "dashed"),
+            plot.caption = element_text(hjust = 0)) + 
+      labs(title = custom_title,
+           x = "", 
+           y = "", 
+           caption = custom_caption) + 
+      scale_color_manual(values = c("Disbursed" = "blue", "Implied Saving" = "orange")) +
+      scale_shape_manual(values = c("Disbursed" = 20, "Implied Saving" = 20)) +
+      guides(color = guide_legend(title = NULL), shape = guide_legend(title = NULL))
+    
+    # Return both the data frame and the chart
+    return(list(data = combined_df, chart = line_chart))
+  }
