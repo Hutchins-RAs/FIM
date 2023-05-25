d9b74ebbf868b11946e7aa079f3a57ad1d79c3d9
nsalwati <87481746+nsalwati@users.noreply.github.com>
2023-05-26 00:40:56 +1100

    fim guide updates
    need to add how to install haver r package

---
+++ fim_guide_knit.R
@@ -0,0 +1,9 @@
+# Load the rmarkdown package
+library(rmarkdown)
+
+# Set the input and output file paths
+input_file <- "fim_guide.Rmd"
+output_file <- "fim_guide.html"
+
+# Render the R Markdown document and save the output to the specified path
+rmarkdown::render(input_file, output_file = output_file)
