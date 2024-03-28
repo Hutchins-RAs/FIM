e52756980201ec788d0d0d2410a1615501ee894c
Lorae <stojanovic.lorae@gmail.com>
2024-03-28 11:14:29 -0400

    fix(fiscal_impact): improve minus_neutral calculation to perform multiplication on growth rates, rather than addition

--- fiscal_impact.R
+++ fiscal_impact.R
@@ -210,7 +210,7 @@ minus_neutral <- function(x, # the data in question
                           rpgg, # real potential gdp growth,
                           cdg # consumption deflator growth
 ) {
-  output <- x - lag(x) * (1 + rpgg + cdg)
+  output <- x - (lag(x) * (1 + rpgg) * (1 + cdg))
   # This optional line will make the 1970 Q1 entries equal a numeric value, rather
   # than NA, by assuming that the 1969 Q4 value for each data series was 0. 
   # Prior versions of the FIM had this setting on for pandemic-era stimulus. We
