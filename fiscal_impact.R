979f33bd2b728d53253fa6be6bfeff84f768e61e
Lorae <stojanovic.lorae@gmail.com>
2024-03-28 13:59:00 -0400

    fix(fiscal_impact): remove extraneous minus_neutral() function from main fiscal_impact script

--- fiscal_impact.R
+++ fiscal_impact.R
@@ -217,16 +217,6 @@ consumption_pt1 <-
   projections %>%
   get_real_levels()
 
-  output <- x - lag(x) * (1 + rpgg + cdg)
-  # This is the correct, calculation, but it affects minus_neutral
-  #output <- x - (lag(x) * (1 + rpgg) * (1 + cdg))
-  # This optional line will make the 1970 Q1 entries equal a numeric value, rather
-  # than NA, by assuming that the 1969 Q4 value for each data series was 0. 
-  # Prior versions of the FIM had this setting on for pandemic-era stimulus. We
-  # have chosen to remove it.
-  # output <- x - lag(x, default = 0) * (1 + rpgg + cdg)
-  return(output)
-}
 # CALCULATE MINUS NEUTRALS
 # Initialize a list of mpcs that are referenced by the function. ui is not included
 # TODO: remove UI from entire workflow - for now this is a patch solution to perfectly
