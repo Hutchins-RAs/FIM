50e4224612737063dd36da8f5c37bc3fd8271484
Lorae <stojanovic.lorae@gmail.com>
2024-04-26 11:39:02 -0400

    docs: note potential mistake and create TODOs for TCJA legislation sunsetting

--- fiscal_impact.R
+++ fiscal_impact.R
@@ -182,8 +182,17 @@ projections <- fim::projections %>%
   ) %>%
   # Construct alternative scenario for personal current taxes, under which the
   # TCJA provisions for income taxes don't expire in 2025
+  # TODO: I think this code has a mistake. Currently, code replaces gfrpt_growth
+  # with the lag of itself starting in 2025 Q3. This doesn't make much sense- 
+  # I think original authors wanted to keep the same growth rate continuing into
+  # perpetuity after 2025 Q3. Figure out what we intend to do here.
+  # TODO: figure out if this growth rate is even used later.
+  #
+  # keep the current law version (where TCJA measures sunset in 2025 Q3)
   mutate(gfrptCurrentLaw = gfrpt,
+         # keep the current law growth
          gfrptCurrentLaw_growth = gfrpt_growth,
+         # redefine the growth to be the lag of itself? (I believe this is a mistake)
          gfrpt_growth =
            if_else(date > yearquarter('2025 Q3'),
                    lag(gfrpt_growth),
