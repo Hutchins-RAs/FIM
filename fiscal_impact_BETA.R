3d48705ae3a286c826b78951f82549a8c6b59342
Lorae <stojanovic.lorae@gmail.com>
2024-04-29 16:38:37 -0400

    feat: create fiscal_impact_BETA code for refactoring

---
+++ fiscal_impact_BETA.R
@@ -0,0 +1,62 @@
+#Section A: prep for new update ----------------------
+
+# Miscellaneous: set up for Eli because has a different library directory -----
+#Get system information
+sys_info <- Sys.info()
+
+# Check if the username is 'easdourian'
+if (sys_info['user'] == 'easdourian') {
+  .libPaths("C:/Users/easdourian/Documents/library")
+  # Other code to run if the user is 'easdourian'
+} else {
+  print("The code is not being run by 'easdourian'")
+}
+
+Sys.setenv(TZ = 'UTC') # Set the default time zone to UTC (Coordinated Universal Time)
+
+librarian::shelf(tidyverse, tsibble, lubridate, glue, TimTeaFan/dplyover, zoo, TTR, fs, gt, openxlsx, 
+                 snakecase, rlang, BrookingsInstitution/ggbrookings) # Load packages
+devtools::load_all() # Load all functions in package
+
+options(digits = 4) # Limit number of digits
+options(scipen = 20)# Turn off scientific notation under 20 digits 
+
+#are we running this after a cbo baseline and pre-bea update?
+post_cbo_baseline<- FALSE
+
+if(post_cbo_baseline == TRUE){
+  month_year <- glue('{format.Date(today() - 7, "%m")}-{year(today())}-post-cbo')
+}else{
+  # Set the value of 'month_year' to the current month and year (in the format "mm-yyyy")
+  month_year <- glue('{format.Date(today() - 7, "%m")}-{year(today())}')
+}
+
+# If the month of the previous month is less than 10, set the value of 'last_month_year' to the previous month and year (in the format "0m-yyyy")
+# Otherwise, set the value of 'last_month_year' to the previous month and year (in the format "mm-yyyy")
+if((month(today() - 7 
+          -months(1)) < 10)){
+  last_month_year <- glue('0{month(today() - 7 -months(1))}-{year(today() - dmonths(1) - dweeks(1))}')
+} else{
+  last_month_year <- glue('{month(today() - 7 -months(1))}-{year(today() - dmonths(1) - dweeks(1))}')
+  
+}
+
+#setting our reference period to be the post-cbo files if we've already produced fim output incorporating the cbo update
+if(file.exists(glue('results/{month_year}-post-cbo'))){
+  last_month_year<- glue('{month_year}-post-cbo')
+}
+
+# Create updatglibe folders
+
+update_in_progress <- TRUE #set this to false if you're not running the code for a new month
+
+if(update_in_progress == TRUE){
+  dir_create(glue('results/{month_year}')) # Create folder for current update in the results directory
+  dir_create(glue('results/{month_year}/input_data')) # Folder to store forecast sheet from current update
+  
+  # Copy the file 'forecast.xlsx' from the 'data' directory to the 'input_data' directory
+  # This is the copy we keep for the current update
+  file_copy(path = 'data/forecast.xlsx', new_path = glue('results/{month_year}/input_data/forecast_{month_year}.xlsx'), overwrite = TRUE)
+}
+
+
