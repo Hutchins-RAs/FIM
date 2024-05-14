3097639c24acd44e00a690f538732690eade8261
Lorae <stojanovic.lorae@gmail.com>
2024-05-14 14:18:23 -0400

    feat: initialize workspace for post_multiplier script

---
+++ multiplier/post_multiplier.R
@@ -0,0 +1,8 @@
+#### STEP 0: Initialize workspace ----------------------------------------------
+library(readxl)
+library(magrittr)
+library(zoo)
+library(dplyr)
+# We assume your current working directory is fim/. If not, please adjust 
+# accordingly before proceeding with code. You can check by running:
+# getwd()
