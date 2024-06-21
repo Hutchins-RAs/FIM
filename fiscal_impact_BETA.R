0fcbb48947e8d20559efa0b3ec4bf6ab855bbda9
Lorae <stojanovic.lorae@gmail.com>
2024-06-21 17:18:34 -0400

    feat: calculate consumption grants contribution

--- fiscal_impact_BETA.R
+++ fiscal_impact_BETA.R
@@ -326,6 +326,7 @@ gdp <- projections$gdp
 ### CALCULATE THE FIM variable-by-variable ########################################
 # Define FIM inputs
 federal_purchases <- projections$federal_purchases
+consumption_grants <- projections$consumption_grants
 
 
 #' Calculate Federal Purchases Contribution
@@ -354,7 +355,6 @@ federal_purchases <- projections$federal_purchases
 #'   rpgg = c(0.005559, 0.005580, 0.005601, 0.005596, 0.005586),
 #'   gdp = c(29314, 29626, 29942, 30268, 30577)
 #' )
-
 federal_purchases_contribution <- function(x, fpdg, rpgg, gdp) {
   # Ensure input vectors are numeric and of the same length
   stopifnot(is.numeric(x), is.numeric(fpdg), is.numeric(rpgg), is.numeric(gdp))
@@ -367,9 +367,46 @@ federal_purchases_contribution <- function(x, fpdg, rpgg, gdp) {
 }
 
 
+#' Calculate Consumption Grants Contribution
+#'
+#' This function calculates the contribution of consumption grants to GDP growth.
+#'
+#' @param x A numeric vector representing the input series for consumption grants,
+#' in billions USD.
+#' @param cgdg A numeric vector representing the consumption grants deflator growth,
+#' as an annualized proportion. For example, 1% annualized consumption grants deflator
+#' growth would be represented as 0.01.
+#' @param rpgg A numeric vector representing the real potential GDP growth, as an
+#' annualized proportion. For example, 3% annualized real potential GDP growth would
+#' be represented as 0.03.
+#' @param gdp A numeric vector representing the GDP, in billions USD.
+#'
+#' @return A numeric vector representing the contribution of consumption grants to
+#' GDP growth.
+#' @export
+#'
+#' @examples
+#' # Example usage:
+#' consumption_grants_contribution(
+#'   x = c(1890.1, 1902.2, 1915.2, 1929.3, 1943.6),
+#'   cgdg = c(0.0064488, 0.0063423, 0.0062568, 0.0061065, 0.0054337),
+#'   rpgg = c(0.005559, 0.005580, 0.005601, 0.005596, 0.005586),
+#'   gdp = c(29314, 29626, 29942, 30268, 30577)
+#' )
+consumption_grants_contribution <- function(x, cgdg, rpgg, gdp) {
+  # Ensure input vectors are numeric and of the same length
+  stopifnot(is.numeric(x), is.numeric(cgdg), is.numeric(rpgg), is.numeric(gdp))
+  stopifnot(length(x) == length(cgdg), length(cgdg) == length(rpgg), length(rpgg) == length(gdp))
+  
+  # Calculate the output
+  output <- 400 * (x - lag(x) * (1 + cgdg + rpgg)) / lag(gdp)
+  
+  return(output)
+}
 
-test <- federal_purchases_contribution(x = federal_purchases,
-                                       fpdg = federal_purchases_deflator_growth,
+
+test <- consumption_grants_contribution(x = consumption_grants,
+                                       cgdg = consumption_grants_deflator_growth,
                                        rpgg = real_potential_gdp_growth,
                                        gdp = gdp) %>%
   as.data.frame()
